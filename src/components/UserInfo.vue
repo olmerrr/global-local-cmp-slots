734e938074c7c3c103b622f235d402e715a7bd6f
Yaroslav <dombrovskiy91@gmail.com>
2020-12-30 14:19:41 +0200

    slots v-slot name slots

--- src/components/UserInfo.vue
+++ src/components/UserInfo.vue
@@ -1,30 +1,28 @@
 <template>
   <section>
-    <header>
-      <h3>{{fullName}}</h3>
-      <base-badge :type="role" :caption="role.toUpperCase()"></base-badge>
-    </header>
-    <p>{{ infoText }}</p>
+    <base-card>
+      <template v-slot:header>
+        <h3>{{ fullName }}</h3>
+        <base-badge :type="role" :caption="role.toUpperCase()"></base-badge>
+      </template>
+      <template v-slot:default>
+        <p>{{ infoText }}</p>
+      </template>
+    </base-card>
   </section>
 </template>
 
 <script>
-  export default {
-  props: ['fullName', 'infoText', 'role'],
-  };
+export default {
+  props: ["fullName", "infoText", "role"],
+};
 </script>
 <style scoped>
-section header{
+/* section header {
   margin: 2rem auto;
   max-width: 30rem;
   border-radius: 12px;
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
   padding: 1rem;
-}
-
-section div {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-}
+} */
 </style>
