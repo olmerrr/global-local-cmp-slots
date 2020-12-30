8c4a1a7ecd743127acf33aa390b9b82b7c113242
Yaroslav <dombrovskiy91@gmail.com>
2020-12-30 12:18:13 +0200

    local+global CMp

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div> 
+    <TheHeader/>
+    <BadgeList/>
+    <UserInfo
+      :full-name="activeUser.name"
+      :info-text="activeUser.description"
+      :role="activeUser.role"
+      />
+  </div>
+</template>
+
+<script>
+import TheHeader from './components/TheHeader.vue';
+import BadgeList from './components/BadgeList.vue';
+import UserInfo from './components/UserInfo.vue';
+
+export default {
+  components: {
+    TheHeader,
+    BadgeList,
+    UserInfo,
+},
+  name: 'App',
+  data() {
+    return {
+      activeUser: {
+        name: 'Yaroslav',
+        description: 'Site owner',
+        role: 'admin'
+      },
+    };
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
